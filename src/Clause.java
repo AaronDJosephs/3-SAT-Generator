fdb5b15464c5bf249ee364a165fb1498a90e5a53
Aaron Josephs <aarondjosephs@gmail.com>
2017-01-05 01:22:17 -0500

    Clauses
    This is where the file should actually be

---
+++ src/Clause.java
@@ -0,0 +1,9 @@
+public class Clause {
+	boolean[] data = new boolean[3];
+	
+	public Clause(boolean first, boolean second, boolean third) {
+		data[0] = first;
+		data[1] = second;
+		data[2] = third;
+	}
+}
