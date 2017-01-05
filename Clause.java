b0105d42465eff621705ab38fcc7d57d7e9011b5
Aaron Josephs <aarondjosephs@gmail.com>
2017-01-05 01:22:26 -0500

    Delete Clause.java

--- Clause.java
+++
@@ -1,9 +0,0 @@
-public class Clause {
-	boolean[] data = new boolean[3];
-	
-	public Clause(boolean first, boolean second, boolean third) {
-		data[0] = first;
-		data[1] = second;
-		data[2] = third;
-	}
-}
