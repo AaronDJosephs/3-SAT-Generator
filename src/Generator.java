3ae3aa66a5fde9e711e8c9cbd7fb5f178a96971f
Aaron Josephs <aarondjosephs@gmail.com>
2017-01-04 22:35:28 -0500

    Added comments for next steps
    main function had the wrong functionality. This was deleted and/or commented out and comments were added describing what needs to be added (which will hopefully happen quite soon)

--- src/Generator.java
+++ src/Generator.java
@@ -13,7 +13,6 @@ public class Generator {
 		else return false;
 	}
 	
-	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("How many clauses do you want? ");
@@ -21,7 +20,20 @@ public class Generator {
 		System.out.println("\n How many variables do you want? ");
 		n = Integer.parseInt(scan.next());
 		float z = (float) m / (float) n;
+		
+		boolean[] variable = new boolean[n]; //Creates an array of booleans of length n. This represents the variables.
+		//how to represent a clause??
+		//in a loop of length m, randomly select 3 variables at a time to add to an array
+		
+		
+		//for(int i = 0; i <= n-1; i++) {
+		//	variable[i] = randomBool();
+		//}
+		
+		
 		System.out.println("\n Your m:n ratio is " + z + ".");
+		//System.out.println("\n Your first and last variables are as follows: " + variable[0] + " " + variable[n-1]);
+		System.out.println("\n The length of the variable list is: " + variable.length);
 	}
 
 }
