028c311dc55d3d854a699316d9c3b0d5b3c8116e
Aaron Josephs <aarondjosephs@gmail.com>
2017-01-06 23:35:10 -0500

    Commented

--- src/Clause.java
+++ src/Clause.java
@@ -1,3 +1,4 @@
+//This class is simply an array of three booleans. That way, a full 3-sat instance can be simply represented by an array of clauses.
 public class Clause {
 	boolean[] data = new boolean[3];
 	
