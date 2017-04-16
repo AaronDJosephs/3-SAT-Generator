946e28fd8ef63820082a6a8360c7002304ed4407
Aaron Josephs <aarondjosephs@gmail.com>
2017-04-17 00:14:00 +1200

    Update Instance.java

--- src/Instance.java
+++ src/Instance.java
@@ -8,18 +8,16 @@ public class Instance {
 	Clause[] instance = new Clause[size];
 	
 	public Instance(Clause[] clauses) {
-		
 		size = clauses.length;
 		clauses = this.clauses;
-		for (int i = 0; i < clauses.length; i++) {
-			for (int j = 0; j < 3; j++) {
+		for (int i = 0; i < clauses.length; i++) 
+		{
+			for (int j = 0; j < 3; j++)
+			{
 				if (Generator.randomBool() == true) 
 					clauses[i].setVar(j, !clauses[i].getVar(j));
 			}
 		}
-		
-		
-	}
-	
+	}	
 }
 
